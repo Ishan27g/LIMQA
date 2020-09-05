cb70bbfed392c74b7bfc3d56b82f1c3bd5c9688d
qunzhi wang <qunzhiw@student.unimelb.edu.au>
2020-09-05 16:52:45 +1000

    backend setup
    sign up and login feature implemented with dummy data

--- server/server.js
+++ server/server.js
@@ -1,16 +1,39 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+
+
+
 const PORT = 8080;
 
+const userRoutes = require('./routes/user-routes');
+const HttpError = require('./models/http-error');
+
+const app = express();
+app.use(bodyParser.json());
 //init mongoDB
-require('./src/db');
+//require('./src/db');
+app.use('/users', userRoutes);
 
 
-app.get('/',(res, rsp) => {
-    rsp.send('E-Portfolio ')
+app.use((req, res, next) => {
+    const error = new HttpError('Could not find this route.', 404);
+    throw error;
+});
+
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+      return next(error);
+    }
+    res.status(error.code || 500)
+    res.json({message: error.message || 'An unknown error occurred!'});
+});
+
+/*app.get('/',(res, rsp) => {
+    rsp.send('E-Portfolio')
 })
 app.listen(PORT, () => {
     require('dns').lookup(require('os').hostname(), function (err, add, fam) {
         console.log(`Express server listening on `+ add + `:${PORT}`)
      })
-})
+})*/
+app.listen(PORT);
