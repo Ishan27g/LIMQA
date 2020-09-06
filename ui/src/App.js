824445af5474ad94ccb88591b4a9fa547baa1094
xmsquared <48476062+xmsquared@users.noreply.github.com>
2020-09-06 13:49:26 +1000

    adding gitignore for node_module

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,13 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Navbar from 'react-bootstrap/Navbar';
+
+import Home from './components/Home/home.js';
+import ManagePage from './components/MangePage/managePage.js';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar>
+          
+        </Navbar>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          111 Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
