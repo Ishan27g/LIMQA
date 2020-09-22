b60e7f75e9a187f93e6f41a08982c4a9853c37c2
Lux040899 <mittallakshya.04@gmail.com>
2020-09-22 12:11:41 +0530

    Updating the username, mobile, address

--- server/controllers/manageController.js
+++ server/controllers/manageController.js
@@ -98,10 +98,148 @@ const updateEmail  = async (req, res, next) => {
   res.status(200).json({ user: user.toObject({ getters: true }) });
 };
 
+const getUsername = async (req, res, next) => {
+  let user = req.user;
+  res.json(
+    {username: user.name}
+  );
+};
 
+const updateUsername  = async (req, res, next) => {
+  const error =  validationResult(req);
+  if(!error.isEmpty()) {
+      console.log(error);
+      return next(new HttpError("Invalid inputs passed, please check your data.", 422));
+  }
+
+  const { name } = req.body;
+
+  let user;
+  try {
+    user = await User.findOne({name: req.user.name});
+  } catch (err) {
+    const error = new HttpError(
+      'Something went wrong, could not update place.',
+      500
+    );
+    return next(error);
+  }
+  if(user) {
+    user.name = name;
+  }
+
+  try {
+    await user.save();
+  } catch (err) {
+    const error = new HttpError(
+      'Something went wrong, could not update place.',
+      500
+    );
+    return next(error);
+  }
+
+  res.status(200).json({ user: user.toObject({ getters: true }) });
+};
+
+const getMobile = async (req, res, next) => {
+  let user = req.user;
+  res.json(
+    {mobile: user.mobile,
+     username: user.name}
+  );
+};
+
+const updateMobile  = async (req, res, next) => {
+  const error =  validationResult(req);
+  if(!error.isEmpty()) {
+      console.log(error);
+      return next(new HttpError("Invalid inputs passed, please check your data.", 422));
+  }
+
+  const { mobile } = req.body;
+  const { officeAddress } = req.body;
+
+  let user;
+  try {
+    user = await User.findOne({moile: req.user.mobile});
+  } catch (err) {
+    const error = new HttpError(
+      'Something went wrong, could not update place.',
+      500
+    );
+    return next(error);
+  }
+  if(user) {
+    user.mobile = mobile;
+    user.officeAddress = officeAddress;
+  }
+
+  try {
+    await user.save();
+  } catch (err) {
+    const error = new HttpError(
+      'Something went wrong, could not update place.',
+      500
+    );
+    return next(error);
+  }
+
+  res.status(200).json({ user: user.toObject({ getters: true }) });
+};
+
+const getAddress = async (req, res, next) => {
+  let user = req.user;
+  res.json(
+    {address: user.officeAddress,
+     username: user.name,
+     email: user.email}
+  );
+};
+
+const updateAddress  = async (req, res, next) => {
+  const error =  validationResult(req);
+  if(!error.isEmpty()) {
+      console.log(error);
+      return next(new HttpError("Invalid inputs passed, please check your data.", 422));
+  }
+
+  const { officeAddress } = req.body;
+
+  let user;
+  try {
+    user = await User.findOne({email: req.user.email});
+  } catch (err) {
+    const error = new HttpError(
+      'Something went wrong, could not update place.',
+      500
+    );
+    return next(error);
+  }
+  if(user) {
+    user.officeAddress = officeAddress;
+  }
+
+  try {
+    await user.save();
+  } catch (err) {
+    const error = new HttpError(
+      'Something went wrong, could not update place.',
+      500
+    );
+    return next(error);
+  }
+
+  res.status(200).json({ user: user.toObject({ getters: true }) });
+};
 
 
 exports.getBioinfo = getBioinfo;
 exports.updateBioinfo = updateBioinfo;
 exports.getEmail = getEmail;
 exports.updateEmail = updateEmail;
+exports.getUsername = getUsername;
+exports.updateUsername = updateUsername;
+exports.getMobile = getMobile;
+exports.updateMobile = updateMobile;
+exports.getAddress = getAddress;
+exports.updateAddress = updateAddress;
--- server/routes/manage-routes.js
+++ server/routes/manage-routes.js
@@ -13,4 +13,16 @@ router.get('/changeEmail', ensureAuthenticated,  manageController.getEmail);
 // expect json data send from front-end.
 router.put('/changeEmail', [ check("email").normalizeEmail().isEmail() ], ensureAuthenticated,  manageController.updateEmail);
 
+router.get('/changeUsername', ensureAuthenticated,  manageController.getUsername)
+// expect json data send from front-end.
+router.put('/changeUsername', [ check("name").not().isEmpty() ], ensureAuthenticated, manageController.updateUsername);
+
+router.get('/changeMobile', ensureAuthenticated,  manageController.getMobile)
+// expect json data send from front-end.
+router.put('/changeMobile', [ check("mobile").not().isEmpty() ], ensureAuthenticated, manageController.updateMobile);
+
+router.get('/changeAddress', ensureAuthenticated,  manageController.getAddress)
+// expect json data send from front-end.
+router.put('/changeAddress', [ check("officeAddress").not().isEmpty() ], ensureAuthenticated, manageController.updateAddress);
+
 module.exports = router;
