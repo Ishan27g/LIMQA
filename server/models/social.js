9a72a26f0d7a2d31abac57a42f9510cf984bb4af
qunzhi wang <qunzhiw@student.unimelb.edu.au>
2020-09-23 11:42:10 +1000

    add account info updates - exclude profile img
    enable user to change name, eamil, social links, address, mobile and supplementary email in one route.

--- server/models/social.js
+++ server/models/social.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const socialSchema = new Schema({
-    name: {type : String, required: true},
+    name: {type : String, required: true},      // social media name
     url: {type : String, required: true, unique: true},
-    owner: { type: mongoose.Types.ObjectId, required: true, ref: 'User'}
+    owner: { type: Schema.Types.ObjectId, required: true, ref: 'User'}
 });
 
 
--- server/routes/manage-routes.js
+++ server/routes/manage-routes.js
@@ -7,22 +7,9 @@ const { ensureAuthenticated } = require('../middlerware/auth');
 
 router.get('/bioinfo', ensureAuthenticated,  manageController.getBioinfo);
 // expect json data send from front-end.
-router.put('/bioinfo', [ check("bioinfo").not().isEmpty() ], ensureAuthenticated,  manageController.updateBioinfo);
+router.put('/bioinfo/:uid', ensureAuthenticated, [ check("bioinfo").not().isEmpty() ], manageController.updateBioinfo);
 
-router.get('/changeEmail', ensureAuthenticated,  manageController.getEmail);
-// expect json data send from front-end.
-router.put('/changeEmail', [ check("email").normalizeEmail().isEmail() ], ensureAuthenticated,  manageController.updateEmail);
-
-router.get('/changeUsername', ensureAuthenticated,  manageController.getUsername)
-// expect json data send from front-end.
-router.put('/changeUsername', [ check("name").not().isEmpty() ], ensureAuthenticated, manageController.updateUsername);
-
-router.get('/changeMobile', ensureAuthenticated,  manageController.getMobile)
-// expect json data send from front-end.
-router.put('/changeMobile', [ check("mobile").not().isEmpty() ], ensureAuthenticated, manageController.updateMobile);
-
-router.get('/changeAddress', ensureAuthenticated,  manageController.getAddress)
-// expect json data send from front-end.
-router.put('/changeAddress', [ check("officeAddress").not().isEmpty() ], ensureAuthenticated, manageController.updateAddress);
-
+router.get('/accSetting/:uid', ensureAuthenticated, manageController.getAcc);
+// expect form data 
+router.put('/accSetting/:uid', ensureAuthenticated, fileUpload.single('profileimg'), manageController.updateAcc);
 module.exports = router;
