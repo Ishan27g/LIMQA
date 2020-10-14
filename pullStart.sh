ae865c13d1b95104d9b338f7e0b578e71733e896
Ubuntu <limqa@limqa.jcpn5me3xfueveqr2c35hjrobe.bx.internal.cloudapp.net>
2020-10-14 08:08:33 +0000

    automatic deployment of LIVE branch, checked every 15 mins

--- pullStart.sh
+++ pullStart.sh
@@ -2,7 +2,7 @@
 now=$(date +"%T")
 echo "------------------------------------------------------" >> /home/limqa/deploy/cronLogs.txt
 echo "Current time : $now" >> /home/limqa/deploy/cronLogs.txt 
-git checkout master >> /home/limqa/deploy/cronLogs.txt
+git checkout LIVE >> /home/limqa/deploy/cronLogs.txt
 if [ "$(git pull)" == "Already up to date." ];
 then 	
 	echo "Already up to date." >> /home/limqa/deploy/cronLogs.txt
