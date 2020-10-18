34ddd3502ad3ad4969189a042ecca2a4710f7e4e
Lux040899 <mittallakshya.04@gmail.com>
2020-10-18 12:13:50 +0530

    Achievement Page

---
+++ ui/src/components/Achievement/AchievementRight.js
@@ -0,0 +1,42 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import Card from "react-bootstrap/Card";
+import docIcon from './documents.png';
+
+class CardRight extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+    name: this.props.name,
+    description: this.props.description,
+    institution: this.props.institution,
+    dateAchieved: this.props.dateAchieved
+    }
+  }
+  render(){   
+    return (
+      <Card style={{
+          position:'relative', 
+          backgroundColor: '#F5F5DC', 
+          width: "1000px",
+          height: "250px", 
+          top: "50px", 
+          margin: "0 auto"}}>
+      <Card.Img src={docIcon} style={{position:'relative', left:"740px", top:"50px"}} width="120" height="150"/>
+      <Card.Body>
+        <Card.Title style = {{position:'relative', left:"140px", bottom:"150px"}}><h2>{this.state.name}</h2></Card.Title>
+        <Card.Text style = {{position:'relative', left:"140px", bottom:"160px", "font-size":"20px", width:"590px" }}>
+        {this.state.description}
+        </Card.Text>
+        <Card.Text style = {{position:'relative', left:"140px", bottom:"160px", "font-size":"20px" }}>
+        <b>Institution</b>: {this.state.institution}
+        <br/>
+        <b>Date</b>: {this.state.dateAchieved}
+        </Card.Text>
+      </Card.Body>
+      </Card>
+    );          
+  }
+}
+  
+export default CardRight;
